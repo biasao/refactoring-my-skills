214d16930e4f90b473592bb01ebff2cd7e0b0c4b
Lucas Damiani <lucasdamiani@gmail.com>
2017-01-21 12:00:38 -0200

    Partial solution for "Candies".

--- assignments/hackerrank/candies/Solution.java
+++ assignments/hackerrank/candies/Solution.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Solution {
 
+  private static final boolean DEBUG = false;
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int numberOfStudents = scanner.nextInt();
@@ -17,28 +19,81 @@ public class Solution {
     for (int student = 0; student < numberOfStudents; student++) {
       int previousStudent = student - 1;
       int nextStudent = student + 1;
-      if (previousStudent >= 0 && rates[student] > rates[previousStudent]) {
-        numberOfCandiesToStudent++;
-      } else if (previousStudent >= 0 && rates[student] <= rates[previousStudent]) {
-        numberOfCandiesToStudent = calculateNumberOfCandiesToStudentRecursively(numberOfCandiesToStudent, numberOfStudents, nextStudent, student, rates);
-      } else if (nextStudent < numberOfStudents && rates[student] > rates[nextStudent]) {
-        numberOfCandiesToStudent++;
-      }
+      numberOfCandiesToStudent = calculateNumberOfCandiesToStudentRecursively(
+                                    numberOfCandiesToStudent,
+                                    numberOfStudents,
+                                    previousStudent,
+                                    nextStudent,
+                                    student,
+                                    rates);
 
-      System.out.println("rate -> " + rates[student] + ", candies -> " + numberOfCandiesToStudent);
+      debug("rate -> " + rates[student] + ", candies -> " + numberOfCandiesToStudent);
       candiesToBuy += numberOfCandiesToStudent;
     }
 
     System.out.println(candiesToBuy);
   }
 
-  private static int calculateNumberOfCandiesToStudentRecursively(int numberOfCandiesToStudent, int numberOfStudents, int nextStudent, int student, int[] rates) {
-    if (nextStudent < numberOfStudents && rates[student] > rates[nextStudent]) {
-      return 1 + calculateNumberOfCandiesToStudentRecursively(numberOfCandiesToStudent, numberOfStudents, nextStudent + 1, nextStudent, rates);
-    } else if (numberOfCandiesToStudent > 0) {
-      return 1;
-    } else {
-      return 1 + Math.abs(numberOfCandiesToStudent);
+  private static int calculateNumberOfCandiesToStudentRecursively(
+      int numberOfCandiesToStudent,
+      int numberOfStudents,
+      int previousStudent,
+      int nextStudent,
+      int student,
+      int[] rates) {
+    // lower boundary
+    if (previousStudent < 0) {
+      debug("lower boundary");
+      return rates[student] < rates[nextStudent] ? 1 : 2;
+    }
+
+    // higher boundary
+    if (nextStudent >= numberOfStudents) {
+      debug("higher boundary");
+      return rates[student] <= rates[previousStudent] ? 1 : numberOfCandiesToStudent + 1;
+    }
+
+    // valley
+    if (rates[student] <= rates[previousStudent] && rates[student] < rates[nextStudent]) {
+      debug("valley");
+      return numberOfCandiesToStudent > 0 ? 1 : 1 + Math.abs(numberOfCandiesToStudent);
+    }
+
+    // climbing up
+    if (rates[student] > rates[previousStudent] && rates[student] <= rates[nextStudent]) {
+      debug("climbing up");
+      numberOfCandiesToStudent += 1;
+      return numberOfCandiesToStudent;
+    }
+
+    // climbing down
+    if (rates[student] <= rates[previousStudent] && rates[student] >= rates[nextStudent]) {
+      debug("climbing down");
+      numberOfCandiesToStudent -= 1;
+      return 1 + calculateNumberOfCandiesToStudentRecursively(
+                    numberOfCandiesToStudent,
+                    numberOfStudents,
+                    student,
+                    nextStudent + 1,
+                    nextStudent,
+                    rates);
+    }
+
+    // hill
+    debug("hill");
+    int candiesForNext = calculateNumberOfCandiesToStudentRecursively(
+                  numberOfCandiesToStudent,
+                  numberOfStudents,
+                  student,
+                  nextStudent + 1,
+                  nextStudent,
+                  rates);
+    return candiesForNext > numberOfCandiesToStudent ? ++candiesForNext : ++numberOfCandiesToStudent;
+  }
+
+  private static void debug(String message) {
+    if (DEBUG) {
+      System.out.println(message);
     }
   }
 
