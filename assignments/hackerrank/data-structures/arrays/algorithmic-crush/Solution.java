89db245697d25d0d5ab12c482d4dcc68052db812
Lucas Damiani <lucasdamiani@gmail.com>
2017-02-12 13:38:20 -0200

    Added solution with O(M+N) time complexity, by using a prefix sum array.

--- assignments/hackerrank/data-structures/arrays/algorithmic-crush/Solution.java
+++ assignments/hackerrank/data-structures/arrays/algorithmic-crush/Solution.java
@@ -4,24 +4,52 @@ import java.util.stream.*;
 
 public class Solution {
 
+    /**
+     * Algorithm Crush can be resolved on O(M + N) time complexity by applying a prefix sum array (see http://wcipeg.com/wiki/Prefix_sum_array_and_difference_array).
+     * The previous solutions were O(MN) time complexity, causing timeout.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int arraySize = scanner.nextInt();
         int operations = scanner.nextInt();
 
-        long[] values = new long[arraySize];
+        long[] prefixSumValues = generatePrefixSumArray(scanner, arraySize, operations);
+        long maxValue = getMaxValue(arraySize, prefixSumValues);
 
-        for (int i = 0; i < operations; i++) {
+        System.out.println(maxValue);
+    }
+
+    private static long[] generatePrefixSumArray(Scanner scanner, int arraySize, int operations) {
+        long[] prefixSumValues = new long[arraySize + 1];
+
+        for (int i = 1; i <= operations; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             long k = scanner.nextLong();
 
-            // Not sure why, but the arrays on this assignment are not 0-based.
-            IntStream.rangeClosed(a, b).forEach(j -> values[j-1] += k);
+            prefixSumValues[a] += k;
+            if (b + 1 <= arraySize) {
+                prefixSumValues[b + 1] -= k;
+            }
         }
 
-        System.out.println(Arrays.stream(values).max().getAsLong());
+        return prefixSumValues;
+    }
+
+    private static long getMaxValue(int arraySize, long[] prefixSumValues) {
+        long maxValue = Long.MIN_VALUE;
+        long[] values = new long[arraySize];
+        long sumOfPrevious = 0;
+        for (int i = 1; i < prefixSumValues.length; i++) {
+            sumOfPrevious += prefixSumValues[i];
+            values[i - 1] = prefixSumValues[0] + sumOfPrevious;
+            if (values[i - 1] > maxValue) {
+                maxValue = values[i - 1];
+            }
+        }
+
+        return maxValue;
     }
 
 }
